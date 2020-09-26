d8e2885a631440dbe0889fa92c2ea4355711e1c7
Philip Wenkel <philip.sve@hotmail.com>
2020-09-26 10:08:50 +0200

    hej

---
+++ helloworld.cpp
@@ -0,0 +1,3 @@
+export module helloworld;
+// import <cstdio>;
+export const char* hello() { return "Hello world!"; }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+import helloworld;  // import declaration
+
+int main() {
+    cout << hello() << endl;
+}
